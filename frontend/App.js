e7f9f38a1ede71d0a0c1d247abb04da0f07f2686
Abdulkader Freij <abdulkader@arch.localdomain>
2020-02-08 21:28:10 +0200

    a

---
+++ frontend/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: "",
+      list: []
+    };
+  }
+  componentDidMount() {
+    fetch("/list/add?")
+      .then(res => res.json())
+      .then(list =>
+        this.setState({ list }
+        )
+      );
+  }
+
+  changeUserInput(input) {
+    this.setState({ task: input });
+  }
+
+  addToList(input) {
+    let newList = this.state.list;
+    newList.push(input);
+    this.setState({ list: newList, task: "" });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          value={this.state.task}
+          onChange={e => this.changeUserInput(e.target.value)}
+        />
+        <button
+          type="submit"
+          onClick={() => this.addToList(this.state.task)}
+        >
+          add
+        </button>
+        <ul>
+          {/* {this.state.list.map(task => (
+            <li>{task.task}</li> */}
+            {console.log(this.state.list)}
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
